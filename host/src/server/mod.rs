21e946456787061a8ad0f9105c627fbe7d46d67f
Champii1 <contact@champii.io>
2024-06-28 13:37:52 +0200

    Fix

--- host/src/server/mod.rs
+++ host/src/server/mod.rs
@@ -21,6 +21,7 @@ async fn process_worker_socket(mut socket: TcpStream) {
             println!("Sent size: {}", data.len() as u64);
             socket.write_all(&data).await.unwrap();
             socket.flush().await.unwrap();
+            socket.shutdown().await.unwrap();
         }
         Err(e) => {
             eprintln!("Error: {:?}", e);
@@ -34,7 +35,6 @@ pub async fn listen_worker() {
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         process_worker_socket(socket).await;
-        socket.shutdown().await?;
 
         break;
     }
