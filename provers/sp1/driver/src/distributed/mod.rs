fa182c5e94fe8f4442b17eacbe4f04ec5685aa8d
Champii1 <contact@champii.io>
2024-07-02 02:51:25 +0200

    Split workers DS into their own module

--- provers/sp1/driver/src/distributed/mod.rs
+++ provers/sp1/driver/src/distributed/mod.rs
@@ -2,7 +2,8 @@ mod orchestrator;
 mod partial_proof_request;
 mod prover;
 mod sp1_specifics;
+mod worker;
 
 pub use partial_proof_request::PartialProofRequest;
 pub use prover::Sp1DistributedProver;
-pub use prover::{WorkerEnvelope, WorkerProtocol, WorkerSocket};
+pub use worker::{WorkerEnvelope, WorkerProtocol, WorkerSocket};
--- provers/sp1/driver/src/distributed/prover.rs
+++ provers/sp1/driver/src/distributed/prover.rs
@@ -1,23 +1,18 @@
-use std::{
-    env,
-    fmt::{Display, Formatter},
-};
+use std::env;
 
 use raiko_lib::{
     input::{GuestInput, GuestOutput},
     prover::{to_proof, Proof, Prover, ProverConfig, ProverResult, WorkerError},
 };
-use serde::{Deserialize, Serialize};
 use sp1_core::{runtime::Program, stark::ShardProof, utils::BabyBearPoseidon2};
 use sp1_sdk::{ProverClient, SP1ProofWithPublicValues, SP1Stdin};
-use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
 
 use crate::{
     distributed::{
         partial_proof_request::PartialProofRequest,
         sp1_specifics::{commit, prove_partial},
     },
-    Sp1Response, ELF,
+    Sp1Response, WorkerProtocol, WorkerSocket, ELF,
 };
 
 pub struct Sp1DistributedProver;
@@ -154,113 +149,5 @@ impl Sp1DistributedProver {
         }
 
         Ok(reachable_ip_list)
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct WorkerEnvelope {
-    pub magic: u64,
-    pub data: WorkerProtocol,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum WorkerProtocol {
-    Ping,
-    PartialProofRequest(PartialProofRequest),
-    PartialProofResponse(Vec<ShardProof<BabyBearPoseidon2>>),
-}
-
-impl Display for WorkerProtocol {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            WorkerProtocol::Ping => write!(f, "Ping"),
-            WorkerProtocol::PartialProofRequest(_) => write!(f, "PartialProofRequest"),
-            WorkerProtocol::PartialProofResponse(_) => write!(f, "PartialProofResponse"),
-        }
-    }
-}
-
-impl From<WorkerProtocol> for WorkerEnvelope {
-    fn from(data: WorkerProtocol) -> Self {
-        WorkerEnvelope {
-            magic: 0xdeadbeef,
-            data,
-        }
-    }
-}
-
-pub struct WorkerSocket {
-    pub socket: tokio::net::TcpStream,
-}
-
-impl WorkerSocket {
-    pub async fn connect(url: &str) -> Result<Self, WorkerError> {
-        let stream = tokio::net::TcpStream::connect(url).await?;
-
-        Ok(WorkerSocket { socket: stream })
-    }
-
-    pub fn new(socket: tokio::net::TcpStream) -> Self {
-        WorkerSocket { socket }
-    }
-
-    pub async fn send(&mut self, packet: WorkerProtocol) -> Result<(), WorkerError> {
-        let envelope: WorkerEnvelope = packet.into();
-
-        let data = bincode::serialize(&envelope)?;
-
-        self.socket.write_u64(data.len() as u64).await?;
-        self.socket.write_all(&data).await?;
-
-        Ok(())
-    }
-
-    pub async fn receive(&mut self) -> Result<WorkerProtocol, WorkerError> {
-        let data = self.read_data().await?;
-
-        let envelope: WorkerEnvelope = bincode::deserialize(&data)?;
-
-        if envelope.magic != 0xdeadbeef {
-            return Err(WorkerError::InvalidMagicNumber);
-        }
-
-        Ok(envelope.data)
-    }
-
-    // TODO: Add a timeout
-    pub async fn read_data(&mut self) -> Result<Vec<u8>, std::io::Error> {
-        // TODO: limit the size of the data
-        let size = self.socket.read_u64().await? as usize;
-
-        let mut data = Vec::new();
-
-        let mut buf_data = BufWriter::new(&mut data);
-        let mut buf = [0; 1024];
-        let mut total_read = 0;
-
-        loop {
-            match self.socket.read(&mut buf).await {
-                // socket closed
-                Ok(n) if n == 0 => return Ok(data),
-                Ok(n) => {
-                    buf_data.write_all(&buf[..n]).await?;
-
-                    total_read += n;
-
-                    if total_read == size {
-                        buf_data.flush().await?;
-
-                        return Ok(data);
-                    }
-
-                    // TODO: handle the case where the data is bigger than expected
-                }
-                Err(e) => {
-                    log::error!("failed to read from socket; err = {:?}", e);
-
-                    return Err(e);
-                }
-            };
-        }
     }
 }
